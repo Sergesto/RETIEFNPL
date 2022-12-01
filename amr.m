b155b38628d44dd776effc785087cb098c8a5344
Sergio A. Merlino Chiozza <104937664+Sergesto@users.noreply.github.com>
2022-12-01 10:53:15 -0300

    Actualizar amr.m

--- amr.m
+++ amr.m
@@ -1,15 +1,20 @@
 %
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
 % Algoritmo de mapeo de retorno
 % Ing. Sergio A. Merlino Chiozza
 %
 function [sn,epn,alfan,ftrial]=amr(epsilonn,ep,alfa,Ee,sy,mK)
 strial=Ee*(epsilonn-ep);
 ftrial=abs(strial)-sy-mK*alfa;
-if ftrial<=0 % paso elástico
+if ftrial<=0 % paso elÃ¡stico
     sn=strial;
     epn=ep;
     alfan=alfa;
-else % paso plástico (mapeo de retorno)
+else % paso plÃ¡stico (mapeo de retorno)
     dgamma=ftrial/(Ee+mK);
     sn=strial-dgamma*Ee*sign(strial);
     epn=ep+dgamma*sign(strial);
